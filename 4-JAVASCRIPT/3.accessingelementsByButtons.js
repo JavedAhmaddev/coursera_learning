0b478e1220e319be6428a717075d79fd0bc72935
JavedAhmaddev <javaedjutt33@gmail.com>
2021-08-14 19:22:23 +0500

    javascript content added

---
+++ 4-JAVASCRIPT/3.accessingelementsByButtons.js
@@ -0,0 +1,19 @@
+
+
+document.getElementById("mybutton").onclick= function(){
+    document.getElementById("text").innerHTML="hello javed";
+    }
+
+document.getElementById("button2").onclick= function(){
+    document.getElementById("text2").innerHTML="Hello JAVED!!  " + document.getElementById("text2").innerHTML + " Awesome";
+    }
+
+document.getElementById("sometextbutton").onclick= function(){
+    document.getElementById("sometext").innerHTML = "Hello JAVED! This text is added by pressing button"; 
+  }
+
+document.getElementById("button4").onclick= function(){
+    document.getElementById("text4").style.color = "red";
+    document.getElementById("text4").style.fontSize = "30px";
+    }
+            
